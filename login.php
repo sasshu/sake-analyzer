6536a99316f6f53807c70034ed6df77c3610878d
sasshu <winter12shu27@gmail.com>
2022-01-17 11:37:28 +0900

    password complete

--- login.php
+++ login.php
@@ -4,7 +4,7 @@ require_unlogined_session();
 
 // 事前に生成したユーザごとのパスワードハッシュの配列
 $hashes = [
-    'hogehoge' => '$2y$10$.kS/1uXTDpH6B6.M236E/.Ey.uQENtT9Pg2Khby7PCPxqRTRntt4m',
+  'cooperator' => password_hash('sake_0117_key', PASSWORD_BCRYPT),
 ];
 
 // ユーザから受け取ったユーザ名とパスワード
@@ -13,32 +13,34 @@ $password = filter_input(INPUT_POST, 'password');
 
 // POSTメソッドのときのみ実行
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (
-        validate_token(filter_input(INPUT_POST, 'token')) &&
-        password_verify(
-            $password,
-            isset($hashes[$username])
-                ? $hashes[$username]
-                : '$2y$10$abcdefghijklmnopqrstuv' // ユーザ名が存在しないときだけ極端に速くなるのを防ぐ
-        )
-    ) {
-        // 認証が成功したとき
-        // セッションIDの追跡を防ぐ
-        session_regenerate_id(true);
-        // ユーザ名をセット
-        $_SESSION['username'] = $username;
-        // ログイン完了後に指定ページに遷移
-        if (isset($_SESSION['url'])) {
-          header("Location: ${_SESSION['url']}");
-          unset($_SESSION['url']);
-        }else {
-          header("Location: ./");
-        }
-        exit;
+  if (
+    validate_token(filter_input(INPUT_POST, 'token')) &&
+    password_verify(
+      $password,
+      isset($hashes[$username])
+        ? $hashes[$username]
+        : '$2y$10$abcdefghijklmnopqrstuv' // ユーザ名が存在しないときだけ極端に速くなるのを防ぐ
+    )
+) {
+      // 認証が成功したとき
+      // セッションIDの追跡を防ぐ
+      session_regenerate_id(true);
+      // ユーザ名をセット
+      $_SESSION['username'] = $username;
+      // ログイン完了後に指定ページに遷移
+      if (isset($_SESSION['url'])) {
+      header("Location: ${_SESSION['url']}");
+      unset($_SESSION['url']);
+      }else {
+        header("Location: ./");
+      }
+      exit;
     }
-    // 認証が失敗したとき
-    // 「403 Forbidden」
-    http_response_code(403);
+  // 認証が失敗したとき
+  // 「403 Forbidden」
+  http_response_code(403);
+
+  header('Content-Type: text/html; charset=UTF-8');
 }
 ?>
 
